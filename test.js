f1c310821ca97bffa01c7c40345f65055f46409c
gladevise <gladevise@gmail.com>
2021-03-08 11:48:57 +0900

    chore: Add inline links and multiple links tests

--- test.js
+++ test.js
@@ -4,6 +4,16 @@ const rlc = require('.')
 
 const doc = fs.readFileSync('fixture.md', 'utf8')
 
+const inlineLinksSample = `
+[example](http://example.com/) is inline link
+
+[remark-link-card](https://www.npmjs.com/package/remark-link-card) is inline link
+`.trim()
+
+const multipleLinksSample = `
+http://example.com/ http://example.com/ http://example.com/
+`.trim()
+
 // Bare links are converted link cards.
 test('convert bare links to link cards', async () => {
   const result = await remark()
@@ -15,5 +25,21 @@ test('convert bare links to link cards', async () => {
 })
 
 // Inline links are not converted to link cards.
+test('Inline links are not converted to link cards.', async () => {
+  const result = await remark()
+    .use(rlc)
+    .process(inlineLinksSample)
+  expect(result.contents.trim()).toEqual(inlineLinksSample)
+
+  // console.log(result.contents);
+})
 
 // Multiple links in one line are not converted to link cards
+test('Multiple links in one line are not converted to link cards', async () => {
+  const result = await remark()
+    .use(rlc)
+    .process(multipleLinksSample)
+  expect(result.contents.trim()).toEqual(multipleLinksSample)
+
+  // console.log(result.contents);
+})
