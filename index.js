88b370d0684ce21ac5ac7b4deac01b730bde3b90
gladevise <gladevise@gmail.com>
2021-03-08 12:17:58 +0900

    feat: Get open graph data with open-graph-scraper

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const visit = require('unist-util-visit')
+const ogs = require('open-graph-scraper')
 const rlc = (options) => {
 
   return async tree => {
+    transformers = []
     visit(tree, 'paragraph', node => {
       visit(node, 'text', textNode => {
         const urls = textNode.value.match(/(https?:\/\/|www(?=\.))([-.\w]+)([^ \t\r\n]*)/g);
         if (urls && urls.length === 1) {
-          node.children[0].type = 'html'
-          node.children[0].value = `<a href="${urls[0]}">${urls[0]}</a>`
+          transformers.push(async () => {
+            try {
+              const { result } = await ogs({ url: urls[0] })
+              console.log(result);
+            } catch (error) {
+              console.log(error);
+            }
+            node.children[0].type = 'html'
+            node.children[0].value = `<a href="${urls[0]}">${urls[0]}</a>`
+          })
         }
       })
     })
+
+    try {
+      await Promise.all(transformers.map(t => t()))
+    } catch (error) {
+      console.error(error);
+    }
 
     return tree
   }
