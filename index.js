1c78acb732e7e968b9be389c1a6da11af31e6e99
gladevise <gladevise@gmail.com>
2021-03-09 16:53:11 +0900

    feat: Output link card HTML

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const visit = require('unist-util-visit')
 const ogs = require('open-graph-scraper')
+
 const rlc = (options) => {
 
   return async tree => {
@@ -10,21 +11,15 @@ const rlc = (options) => {
         if (urls && urls.length === 1) {
           transformers.push(async () => {
 
-            // get open graph data
             try {
+              // get open graph data
               const { result } = await ogs({ url: urls[0] })
-              console.log(result);
+              node.children[0].type = 'html'
+              node.children[0].value = createHtml(urls[0], result)
             } catch (error) {
               console.log(error);
             }
 
-            // get favicon src
-            parsedUrl = new URL(urls[0])
-            const faviconSrc = `https://www.google.com/s2/favicons?domain=${parsedUrl.hostname}`
-            console.log(faviconSrc);
-
-            node.children[0].type = 'html'
-            node.children[0].value = `<a href="${urls[0]}">${urls[0]}</a>`
           })
         }
       })
@@ -38,6 +33,35 @@ const rlc = (options) => {
 
     return tree
   }
+}
+
+const createHtml = (targetUrl, ogResult) => {
+  // setting fallbacks
+  const parsedUrl = new URL(targetUrl)
+  const title = ogResult?.ogTitle || parsedUrl.hostname
+  const descriptionElement = ogResult?.ogDescription ?
+    `<div class="rlc-description">${ogResult.ogDescription}</div>` : '';
+  const faviconSrc = `https://www.google.com/s2/favicons?domain=${parsedUrl.hostname}`
+  const imageElement = ogResult?.ogImage?.url ?
+    `<div class="rlc-image-container">
+      <image class="rlc-image" src="${ogResult.ogImage.url}" alt="${ogResult.ogImage?.alt || title}" width="100%" height="100%"/>
+    </div>`
+    : '';
+
+  const outputHTML = `
+<a class="rlc-container" href="${targetUrl}">
+  <div class="rlc-info">
+    <div class="rlc-title">${title}</div>
+    ${descriptionElement}
+    <div class="rlc-url-container">
+      <img class="rlc-favicon" src="${faviconSrc}" alt="${title} favicon" width="16px" height="16px">
+      <span class="rlc-url">${targetUrl}</span>
+    </div>
+  </div>
+  ${imageElement}
+</a>
+`.trim()
+  return outputHTML
 }
 
 module.exports = rlc
